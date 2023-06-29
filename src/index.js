ce70f7d40cec4a0a07616ab45492ae0f424db8b4
MostafaAE <mostafa.aymanae@gmail.com>
2023-06-29 23:37:53 +0300

    Add API props to star rating component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating maxRating={10} defaultRating={5} />
+    <StarRating
+      maxRating={5}
+      color='red'
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
   </React.StrictMode>
 );
